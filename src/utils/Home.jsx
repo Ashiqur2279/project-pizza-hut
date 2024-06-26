0924f6231892053c332217b24693b4a11b65d2d4
ashiqur2279 <ashiqurrahman2279@gmail.com>
2024-06-26 10:05:28 +0600

    implement the home file

---
+++ src/utils/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>
+        Pizza Hut <br />
+        The Perfect Slice for Every Occasion.
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
