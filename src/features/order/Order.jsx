f0dbb2a7f507150c59dfa24a11dc518fdfb32ece
ashiqur2279 <ashiqurrahman2279@gmail.com>
2024-07-01 21:14:18 +0600

    implement the EmptyCart, Order, OrderItem and indexCSS file

---
+++ src/features/order/Order.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const order = {
+  id: "ABCDEF",
+  customer: "ashiqur",
+  phone: "43257366981",
+  address: "Dighirhat, Hatibandha",
+  priority: true,
+  estimatedDelivery: "2027-04-25T10:00:00",
+  cart: [
+    {
+      pizzaId: 7,
+      name: "Napoli",
+      quantity: 3,
+      unitPrice: 16,
+      totalPrice: 48,
+    },
+    {
+      pizzaId: 5,
+      name: "Diavola",
+      quantity: 2,
+      unitPrice: 16,
+      totalPrice: 32,
+    },
+    {
+      pizzaId: 3,
+      name: "Romana",
+      quantity: 1,
+      unitPrice: 15,
+      totalPrice: 15,
+    },
+  ],
+  position: "-9.000,38.000",
+  orderPrice: 95,
+  priorityPrice: 19,
+};
+
+const Order = () => {
+  return <div></div>;
+};
+
+export default Order;
