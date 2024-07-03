4622b145ff640f62ebe42b50f91a9dcd2cce6fd0
ashiqur2279 <ashiqurrahman2279@gmail.com>
2024-07-03 10:18:00 +0600

    implement a loader functionalities when the menuItem is loading

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,16 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import CartOverview from "../features/cart/CartOverview";
 import Header from "./Header";
+import Loader from "./Loader";
 
 const AppLayout = () => {
+  const navigate = useNavigation();
+  const isLoading = navigate.state === "loading";
+
   return (
-    <>
+    <div className="layout">
       <Header />
-      <main>
-        <Outlet />
-      </main>
+      <main>{isLoading ? <Loader /> : <Outlet />}</main>
       <CartOverview />
-    </>
+    </div>
   );
 };
 
